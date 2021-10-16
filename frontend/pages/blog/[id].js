f1c6384db4671a4a9945a355f8250a8d6cf8c6a3
tommy-wt <10mmywato@gmail.com>
2021-10-16 22:32:33 +0900

    追加: frontend ソース
    Nextjsのサンプルブログ回収中のコード一式追加

---
+++ frontend/pages/blog/[id].js
@@ -0,0 +1,36 @@
+import { client } from "../../libs/client";
+import styles from "./Blogid.module.css";
+
+export default function Blogid({ blog }) {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.h1}>{blog.title}</h1>
+      <p className={styles.publishedAt}>{blog.publishedAt}</p>
+      <div
+        className={styles.post}
+        dangerouslySetInnerHTML={{
+          __html: `${blog.body}`
+        }}
+      />  
+    </main>
+  )
+}
+
+export async function getStaticPaths(){
+  const data = await client.get({ endpoint: "blog"});
+
+  const paths = data.contents.map((content) => { return { params: {"id": content.id}} });
+  // console.log(paths)
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps(context){
+  const id = context.params.id;
+  const data = await client.get({ endpoint: 'blog', contentId: id});
+  
+  return {
+    props: {
+      blog: data,
+    },
+  };
+}
---
+++ frontend/pages/index.js
@@ -0,0 +1,40 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
+import Footer from '../components/footer'
+import { client } from "../libs/client"
+import { Buton, Button, ButtonGroup } from '@mui/material'
+import Blog from "../components/blog/Blog"
+
+export default function Home( { blog, cms } ) {
+  // console.log(blog.blog)
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <Blog blog={blog}></Blog>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export const getStaticProps = async() => {
+  const data = await client.get({ endpoint: "blog"});
+  return {
+    props: {
+      blog: data.contents,
+      cms: {
+        posts: data.contents.map((post) => post.title)
+      }
+    }
+  }
+  // return {
+  //   props: {
+  //     blog: data.contents
+  //   }
+  // };
+}

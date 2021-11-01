8aabd44aface49e677566206264b5ee450414d54
tommy-wt <10mmywato@gmail.com>
2021-11-01 23:22:27 +0900

    修正: AboutImageListレスポンシブ対応

--- frontend/components/about/AboutImages.js
+++ frontend/components/about/AboutImages.js
@@ -1,14 +1,23 @@
+import * as React from 'react';
 import { ImageList } from "@mui/material";
 import { ImageListItem } from "@mui/material";
 import { Grid } from "@mui/material";
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 export default function AboutImages({ aboutImages }) {
+    let colsNumResult = 2;
+    const theme = useTheme();
+    if(useMediaQuery(theme.breakpoints.up('md'))){
+        colsNumResult = 3;
+    }
+
     return (
         <Grid container spaceing={1}>
             <Grid item xs={12} md={3}>
             </Grid>
             <Grid item xs={12} md={9}>
-                <ImageList cols={3}>
+                <ImageList cols={colsNumResult}>
                     {aboutImages.map((item) => (
                         <ImageListItem sx={{ width: "100%", height: "auto" }}>
                             <img
